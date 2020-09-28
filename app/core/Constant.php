0daffdedbe82c5e01cce775dbf7a8af411d3d728
abeey <abeey.f11@gmail.com>
2020-09-28 14:27:38 +0700

    baseurl

---
+++ app/core/Constant.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/_latihan/php-mvc/public');
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page</title>
+
+    <link rel="stylesheet" href="<?= BASEURL ?>/css/bootstrap.css">
 </head>
 
 <body>
