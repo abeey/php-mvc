f1e7b7870b0b28c4cbf27cdc7c7c1ee1f9a5629c
abeey <abeey.f11@gmail.com>
2020-09-28 14:37:45 +0700

    models

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,10 @@ class Home extends Controller
 {
     function index()
     {
-        $this->view('home/index');
+        $data['nama'] = $this->model('User_m')->getUser();
+
+        $this->view('templates/header');
+        $this->view('home/index', $data);
+        $this->view('templates/footer');
     }
 }
---
+++ app/models/User_m.php
@@ -0,0 +1,9 @@
+<?php
+class User_m
+{
+    private $nama = "Febri Nahrul";
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
